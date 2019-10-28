429f3c13b73cfae5d9be29489ea2ff7c0cefa6ad
keranna <sonia.carier@keranna.qc.ca>
2019-10-28 17:31:29 -0400

    bscs.py

--- testing/basics.py
+++ testing/basics.py
@@ -1,31 +1,16 @@
-#!/usr/bin/env python3
-# An EV3 Python (library v2) solution to Exercise 3
-# of the official Lego Robot Educator lessons that
-# are part of the EV3 education software
-
-from ev3dev2.motor import MoveTank, OUTPUT_B, OUTPUT_C
-from ev3dev2.sensor.lego import ColorSensor
-from ev3dev2.button import Button
-from time import sleep
-
-btn = Button(port.S4) # we will use any button to stop script
-tank_pair = MoveTank(OUTPUT_B, OUTPUT_C)
-
-# Connect an EV3 color sensor to any sensor port.
-cl = ColorSensor(Port.S2)
-
-while not btn.any(Port.S4):    # exit loop when any button pressed
-    # if we are over the black line (weak reflection)
-    if cl.reflected_light_intensity<30:   
-        # medium turn right
-        tank_pair.on(left_speed=50, right_speed=0)
-    
-    else:   # strong reflection (>=30) so over white surface
-        # medium turn left
-        tank_pair.on(left_speed=0, right_speed=50)
-    
-    sleep(0.1) # wait for 0.1 seconds
+from pybricks import ev3brick as brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import (Port, Stop, Direction, Button, Color,
+                                 SoundFile, ImageFile, Align)
+from pybricks.tools import print, wait, StopWatch
+from pybricks.robotics import DriveBase
 
 
+brick.sound.beep()
 
+CL =  ColorSensor(Port.S2)
 
+RFLx = CL.reflection()
+
+while RFLx < 60
